48f5bf4bf1256049a329a7ed5f746057e2facc71
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2025-06-16 15:29:12 -0300

    c05 ex00 ft_iterative_factorial

---
+++ c05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	num;
+
+	num = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 1)
+		num *= nb--;
+	return (num);
+}
+
+int	main(void)
+{
+	printf("fatorial de 3: %d\n", ft_iterative_factorial(3)); // 6
+	printf("fatorial de 6: %d\n", ft_iterative_factorial(6)); // 720
+	printf("fatorial de 10: %d\n", ft_iterative_factorial(10)); // 3628800
+	printf("fatorial de -2: %d\n", ft_iterative_factorial(-2)); // 0
+	printf("fatorial de 0: %d\n", ft_iterative_factorial(0)); // 1
+	printf("fatorial de 1: %d\n", ft_iterative_factorial(1)); // 1
+
+	// estoura o limite do tipo inteiro
+	// printf("fatorial de 20: %d\n", ft_recursive_factorial(20));
+	return (0);
+}
